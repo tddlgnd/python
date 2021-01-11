db6533364e403d730a9b15b9bbf9c352dcae2297
peterchokr <61734237+peterchokr@users.noreply.github.com>
2021-01-11 18:45:39 +0900

    change src

---
+++ src/chap06/pytha_tri_.py
@@ -0,0 +1,10 @@
+﻿new_list = [(x,y,z) for x in range(1,30) for y in range(x,30) for z in range(y,30) if x**2 + y**2 == z**2]
+print(new_list)
+
+new_list = []
+for x in range(1, 30):
+	for y in range(x, 30):
+		for z in range(y, 30):
+		    if x**2+y**2==z**2:
+		        new_list.append((x, y, z))
+print(new_list)
